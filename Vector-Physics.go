b600c0452bcb047245b1106c68db621ecbb21096
Jibble330 <jettb6@gmail.com>
2022-08-25 22:08:23 -0700

    Get working menu

--- Vector-Physics.go
+++ Vector-Physics.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+    "os"
 	"fmt"
 	"log"
 	"math"
 	"strings"
-	"time"
 
 	"github.com/gdamore/tcell"
 )
@@ -15,6 +15,10 @@ const RAD_TO_DEG = 180/math.Pi
 var (
     screen tcell.Screen
     err error
+
+    defStyle = tcell.StyleDefault
+    invertStyle = tcell.StyleDefault.Reverse(true)
+    Copied Vector
 )
 
 type Vector struct {
@@ -45,40 +49,88 @@ Magnitude: %v
 Degrees: %v`, v.X, v.Y, v.Mag, v.Deg)
 }
 
+func (v *Vector) Add(u Vector) Vector {
+    return XY(v.X+u.X, v.Y+u.Y)
+}
+
+func (v *Vector) Scaled(mult float64) Vector {
+    return Vector{v.X*mult, v.Y*mult, v.Mag*mult, v.Deg}
+}
+
 func listen(events chan tcell.Event) {
     for {
         ev := screen.PollEvent()
         events <- ev
+        switch ev := ev.(type) {
+        case *tcell.EventKey:
+            if ev.Key() == tcell.KeyESC {
+                Exit()
+            }
+        }
     }
 }
 
-func WriteString(input string, pos [2]int, bg, fg tcell.Color) {
+func WriteString(input string, x, y int, style tcell.Style) {
     lines := strings.Split(input, "\n")
 
     for lineIndex, line := range lines {
         for charIndex, char := range line {
-            X := charIndex+pos[0]
-            Y := lineIndex+pos[1]
-            style := tcell.StyleDefault.Background(bg).Foreground(fg)
+            X := charIndex+x
+            Y := lineIndex+y
 
             screen.SetContent(X, Y, char, nil, style)
         }
     }
 }
 
-func menu[T any](options []T) uint {
+func menu(options []string) uint {
     events := make(chan tcell.Event)
     go listen(events)
 
     var choice uint
 
     for ev := range events {
-        switch ev.(type) {
+        switch ev := ev.(type) {
         case *tcell.EventKey:
-
+            key := ev.Key()
+            switch key {
+            case tcell.KeyUp:
+                if choice > 0 {
+                    choice--
+                }
+            case tcell.KeyDown:
+                if choice < uint(len(options))-1 {
+                    screen.Clear()
+                    choice++
+                }
+            case tcell.KeyEnter:
+                return choice
+            }
+            centerX, centerY := screen.Size()
+            centerX /= 2
+            centerY /= 2
+            startY := centerY-len(options)
+            for index, option := range options {
+                startX := centerX-(3+len(option)/2)
+                var numStyle tcell.Style
+                if choice == uint(index) {
+                    numStyle = invertStyle
+                } else {
+                    numStyle = defStyle
+                }
+                WriteString(fmt.Sprint(index+1, "."), startX, startY+index, numStyle)
+                WriteString(" "+ option, startX+3, startY+index, defStyle)
+                screen.Show()
+            }
         }
     }
     return choice
+}
+
+func Exit() {
+    screen.Clear()
+    screen.Fini()
+    os.Exit(0)
 }
 
 func main() {
@@ -90,13 +142,12 @@ func main() {
     if err = screen.Init(); err != nil {
         log.Fatal(err)
     }
-    defer screen.Fini()
+    defer Exit()
 
-    defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
     screen.SetStyle(defStyle)
 
-    screen.SetContent(0, 0, 'H', nil, defStyle)
-    screen.Show()
-    
-    time.Sleep(time.Second*5)
+    choice := menu([]string{"Add Vectors", "Scale Vectors", "Dot Product", "Cross Product", "Project Vectors"})
+    switch choice {
+        
+    }
 }
